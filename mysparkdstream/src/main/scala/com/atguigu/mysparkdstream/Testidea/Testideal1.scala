a72d8003768e31e151840d185477796a1472b1ab
lzp <lzp.123456@qq.com>
2019-12-13 22:23:08 +0800

    Testgit3

---
+++ mysparkdstream/src/main/scala/com/atguigu/mysparkdstream/Testidea/Testideal1.scala
@@ -0,0 +1,7 @@
+package com.atguigu.mysparkdstream.Testidea
+
+class Testideal1 {
+  def main(args: Array[String]): Unit = {
+    print("Testideal1")
+  }
+}
---
+++ mysparkdstream/src/main/scala/com/atguigu/mysparkdstream/Testidea/Testideal2.scala
@@ -0,0 +1,7 @@
+package com.atguigu.mysparkdstream.Testidea
+
+class Testideal2 {
+  def main(args: Array[String]): Unit = {
+    print("Testideal2")
+  }
+}
