2cc56658ababc1ba22c5df81425ece767baeb068
lzp <lzp.123456@qq.com>
2019-12-13 21:57:26 +0800

    update2

---
+++ mysparkdstream/src/main/scala/com/atguigu/mysparkdstream/mytestgit2/Test21.scala
@@ -0,0 +1,7 @@
+package com.atguigu.mysparkdstream.mytestgit2
+
+class Test21 {
+  def main(args: Array[String]): Unit = {
+    print("helloworld2")
+  }
+}
---
+++ mysparkdstream/src/main/scala/com/atguigu/mysparkdstream/mytestgit2/Test22.scala
@@ -0,0 +1,7 @@
+package com.atguigu.mysparkdstream.mytestgit2
+
+class Test22 {
+  def main(args: Array[String]): Unit = {
+    print("test2222")
+  }
+}
