c6bff698db712f6f6a4426d13e810e9b65c36001
lzp <lzp.123456@qq.com>
2019-12-13 22:07:11 +0800

    update3

---
+++ mysparkdstream/src/main/scala/com/atguigu/mysparkdstream/TestGit3/TestGit31.scala
@@ -0,0 +1,7 @@
+package com.atguigu.mysparkdstream.TestGit3
+
+class TestGit31 {
+  def main(args: Array[String]): Unit = {
+    print("Testgit3")
+  }
+}
---
+++ mysparkdstream/src/main/scala/com/atguigu/mysparkdstream/TestGit3/Testgit32.scala
@@ -0,0 +1,9 @@
+package com.atguigu.mysparkdstream.TestGit3
+
+class Testgit32 {
+  def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
+      print("Testgit32")
+    }
+  }
+}
